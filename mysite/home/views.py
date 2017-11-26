1a3b484134e899d0ab9e6f581ed924d1afefaf60
Jeremy Adams <jadams@olw-jadams.local>
2017-11-26 09:36:45 -0500

    use of webapp config file to handle configurations for different deployments of webapp

--- mysite/home/views.py
+++ mysite/home/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     """index page view for home app"""
-    return HttpResponse("Hello, world. You're at the home index. Greetings Rick.")
+    return HttpResponse("Welcome to the home index.")
