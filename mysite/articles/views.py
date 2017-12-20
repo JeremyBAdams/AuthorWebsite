a40b959b6e4c0f37c0952a3f0a05f9c090bcf23d
Jeremy Adams <jadams@olw-jadams.oicr.on.ca>
2017-12-20 13:14:47 -0500

    POC of serving css files. Created class to handle archetype colors

--- mysite/articles/views.py
+++ mysite/articles/views.py
@@ -1,6 +1,40 @@
+from django.conf import settings
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from applib.articles.article_manager import ArticleManager
+
+am = ArticleManager()
+
+css_dir = am.get_article_css_dir()
+site_css = css_dir+"/site.css"
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Index page for articles app")
+    response_string = """<!DOCTYPE HTML>
+<head>
+<link rel="stylesheet" type ="text/css" href="%s">
+</head>
+<body>
+    <div class="my_div">
+    Index page for articles app<br>
+    static root is: %s<br>
+    static url is: %s<br>
+    css dir is: %s<br>
+    warden css is: %s<br>
+    rebel css is: %s<br>
+    champion css is: %s<br>
+    architect css is: %s<br>
+    conqueror css is: %s<br>
+    exile css is: %s<br>
+    </div>
+</body>
+""" % (site_css,settings.STATIC_ROOT,settings.STATIC_URL,css_dir,
+    am.get_archetype_css_file(am.WARDEN),
+    am.get_archetype_css_file(am.REBEL),
+    am.get_archetype_css_file(am.CHAMPION),
+    am.get_archetype_css_file(am.ARCHITECT),
+    am.get_archetype_css_file(am.CONQUEROR),
+    am.get_archetype_css_file(am.EXILE)
+)
+    return HttpResponse(response_string)
