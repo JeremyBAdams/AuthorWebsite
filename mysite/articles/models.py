9671633689f420c714a9a4da0ec987733ebc0a75
Jeremy Adams <jadams@olw-jadams.oicr.on.ca>
2018-02-06 11:51:56 -0500

    reset database and migrations. developed new schema with proper primary keys and made new initial migration

--- mysite/articles/models.py
+++ mysite/articles/models.py
@@ -8,30 +8,36 @@ class Pseudonym(models.Model):
     """class for pseudo-authors/writers of articles"""
 
     #all articles must be written by a pseudo author (via foreignkey constraint)
+    pseudonym_id = models.AutoField(
+        primary_key = True
+    )
     pseudonym = models.CharField(
-        primary_key = True,
         unique = True,
         max_length=250,
     )
 
     def __str__(self):
         """docstring"""
-        return "Pseudonym instance - " + self.pseudonym
+        return "Pseudonym instance - " + self.pseudonym \
+               + ", id: " + str(self.pseudonym_id)
 
 class Series(models.Model):
     """class for the serialization group that an article falls under"""
 
     #all articles fall into a series (eg. Astran History, or Character Bios)
     #articles will be forced into a series via foreignkey constraint)
+    series_id = models.AutoField(
+        primary_key = True
+    )
     series_name = models.CharField(
-        primary_key = True,
         unique=True,
         max_length=250
     )
 
     def __str__(self):
         """docstring"""
-        return "Series instance - " + self.series_name
+        return "Series instance - " + self.series_name \
+               + ", id: " + str(self.series_id)
 
 class Palette(models.Model):
     """class for webpage color palette based on the novel's concept of soul archetypes"""
@@ -40,20 +46,26 @@ class Palette(models.Model):
     #the best soul archetype that covers the article's content will be used
 
     #color palette archetype name
+    palette_id = models.AutoField(
+        primary_key = True
+    )
     archetype_name = models.CharField(
-        primary_key = True,
         unique=True,
         max_length=20
     )
 
     def __str__(self):
         """docstring"""
-        return "Palette instance - " + self.archetype_name
+        return "Palette instance - " + self.archetype_name \
+               + ", id: " + str(self.palette_id)
 
 class Article(models.Model):
     """class for story-based articles"""
 
     "attributes"
+    article_id = models.AutoField(
+        primary_key = True
+    )
     #title: article title
     title = models.CharField(
         max_length=500,
@@ -69,7 +81,6 @@ class Article(models.Model):
     #ie '123456', to be used for file pathing
     #html pages and images will be named based on reference id number
     reference_number = models.CharField(
-        primary_key=True,
         validators=[MinLengthValidator(6)],
         max_length=6,
         unique=True
